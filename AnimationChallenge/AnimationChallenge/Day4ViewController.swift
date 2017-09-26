91f91984340788ee253afeba1492117978bb67cd
Nataliia Koldaeva <natacodes@gmail.com>
2017-09-25 17:04:13 -0700

    Added navigation system in app

---
+++ AnimationChallenge/AnimationChallenge/Day4ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  Day4ViewController.swift
+//  AnimationChallenge
+//
+//  Created by Nataliia Koldaeva on 9/25/17.
+//  Copyright © 2017 Nataliia Koldaeva. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Day4ViewController: UIViewController {
+    
+}
---
+++ AnimationChallenge/AnimationChallenge/FirstViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FirstViewController.swift
+//  AnimationChallenge
+//
+//  Created by Nataliia Koldaeva on 9/25/17.
+//  Copyright © 2017 Nataliia Koldaeva. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FirstViewController: UIViewController {
+    
+}
